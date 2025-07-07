f9f02fdfca81374298d169badf4f40598777870d
unknown <kelvinortiz2631@gmail.com>
2025-07-06 20:05:22 -0400

    add  routes import to server

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { Server } from "./presentation";
 import { envs } from "./config/envs";
+import { AppRoutes } from "./presentation/routes";
 (async () => {
   main();
 })();
@@ -8,6 +9,7 @@ async function main() {
   const server = new Server({
     port: envs.PORT,
     publicPath: envs.PUBLIC_PATH,
+    routes: AppRoutes.routes,
   });
 
   await server.start();
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,9 @@
-import express from "express";
+import express, { Router } from "express";
 import path from "path";
 
 interface Options {
   port: number;
+  routes: Router;
   publicPath?: string;
 }
 
@@ -10,16 +11,25 @@ export class Server {
   private readonly app = express();
   private readonly port: number;
   private readonly publicPath: string;
+  private readonly routes: Router;
   constructor(options: Options) {
-    const { port, publicPath } = options;
+    const { port, routes, publicPath } = options;
     this.port = port;
     this.publicPath = publicPath ?? "public";
+    this.routes = routes;
   }
   async start() {
+    //* Middlewares
+    this.app.use(express.json()); // raw
+    this.app.use(express.urlencoded({ extended: true })); // x-www-form-urlencoded
+    //* Public Folder
     this.app.use(express.static(this.publicPath));
 
+    //* Routes
+    this.app.use(this.routes);
+
     this.app.get(/.*/, (req, res) => {
-      //const indexPath = path.join(__dirname + "../../public/index.html");
+      //const indexPath = path.join(__dirname + "../../${ this.publicPath }/index.html");
       res.sendFile("index.html", { root: this.publicPath });
     });
 
